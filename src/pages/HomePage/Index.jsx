5ce78cc6eba0286267127f12d409811fe73b3b82
eduardorossetti <edurossettimelo@hotmail.com>
2023-09-28 20:56:10 -0300

    atividade3

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -1,61 +1,43 @@
 import { Card, Row, Container, Col } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faFile, faBell, faGear } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFilePen,
+  faUserPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import Logo from "./img/img_aprata.png";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-
   return (
     <Container className="mt-3">
       <div className="d-flex justify-content-center align-center">
-        <img src={Logo} alt="" />
+        <img src={Logo} alt="logo" className="img-fluid" />
       </div>
-      <h2>Bem vindos, usuários!</h2>
-      <Row className="mt-3">
-        <Col className="p-2 col-3">
-          <Link to="/">
-            <Card>
-              <Card.Body className=" bg-light card">
-                <h3 className="text-center">Cadastros</h3>
-                <FontAwesomeIcon icon={faPenToSquare} size="2xl" />
+
+      <Row className="mt-3 justify-content-center align-center">
+        <Col className="p-2 col-md-2 col-sm-6">
+          <Link to="/project-pfsii-react/cadastro/cargos">
+            <Card className="bg-light">
+              <Card.Body className="text-center">
+                <h5>Cargos</h5>
+                <FontAwesomeIcon icon={faFilePen} size="2x" />
               </Card.Body>
             </Card>
           </Link>
         </Col>
-        <Col className="p-2 col-3">
-          <Link to="/">
-            <Card >
-              <Card.Body className=" bg-light card">
-                <h3 className="text-center">Relatórios</h3>
-                <FontAwesomeIcon icon={faFile} size="2xl" />
-              </Card.Body>
-            </Card>
-          </Link>
-        </Col>
-        <Col className="p-2 col-3">
-          <Link to="/">
-            <Card>
-              <Card.Body className="bg-light card">
-                <h3 className="text-center">Notificações</h3>
-                <FontAwesomeIcon icon={faBell} size="2xl" />
-              </Card.Body>
-            </Card>
-          </Link>
-        </Col>
-        <Col className="p-2 col-3">
-          <Link to="/">
-            <Card>
-              <Card.Body className="bg-light card">
-                <h3 className="text-center">Configurações</h3>
-                <FontAwesomeIcon icon={faGear} size="2xl" />
+        <Col className="p-2 col-md-2 col-sm-6">
+          <Link to="/project-pfsii-react/cadastro/funcionarios">
+            <Card className="bg-light">
+              <Card.Body className="text-center">
+                <h5>Funcionarios</h5>
+                <FontAwesomeIcon icon={faUserPlus} size="2x" />
               </Card.Body>
             </Card>
           </Link>
         </Col>
       </Row>
     </Container>
-  )
+  );
 };
 
 export default Home;
